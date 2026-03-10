2caec77d6d983ca96e7b79d508a8f9f1292a2970
Thainá Letícia Dias da Costa <thainadiasc@gmail.com>
2026-03-10 20:13:09 -0300

    Update Tarefa.cs

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,16 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Titulo { get; set; }
+
         public string Descricao { get; set; }
+
         public DateTime Data { get; set; }
-        public EnumStatusTarefa Status { get; set; }
+
+        public string Status { get; set; }  // Pendente, Concluída etc.
     }
 }
